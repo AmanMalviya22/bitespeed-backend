7a849ca3cfd04a62b9b95d41b48410de73254fde
AmanMalviya22 <amanaman26802@gmail.com>
2024-07-03 11:46:15 +0530

    updated connection string

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use('/identify', identifyRouter);
+app.get('/', (req, res) => {
+    res.send(`<h1 style="color:red">Hello, this is the Bitespeed Backend API!</h1>`);
+  });
 
-mongoose.connect('mongodb://localhost:27017/bitespeed', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://aman:a#1009998@A@bitespeed.xau2fam.mongodb.net/?retryWrites=true&w=majority&appName=bitespeed', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(port, () => console.log(`App running on port ${port}`)))
   .catch(err => console.error(err));
